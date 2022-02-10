a789b7e43ba5bc7c2a551d09aef2e6ba3337c5c3
Jamshid91 <iymon0217@gmail.com>
2022-02-10 22:35:36 +0500

    added

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,13 @@ const burger = document.querySelector('.burger'),
       mobileMenu = document.querySelector('.mobile-menu'),
       closeMenu = document.querySelector('.close__menu'),
       wrapper = document.querySelector('.wrapper'),
-      headerItems = document.querySelectorAll('.header-item')
+      headerItems = document.querySelectorAll('.header-item'),
+      debtLists = document.querySelectorAll('.debt-list'),
+      debt = document.querySelector('.debt'),
+      wrapperFormInp = document.querySelector('.wrapper-form__inp'),
+      sliderBtns = document.querySelectorAll('.slider-btns button'),
+      forIndividuals = document.querySelector('.for-individuals'),
+      forLegalEntities = document.querySelector('.for-legal-entities')
 
 burger.addEventListener('click', () => {
   mobileMenu.classList.add('showMenu');
@@ -12,11 +18,31 @@ closeMenu.addEventListener('click', () => {
   mobileMenu.classList.remove('showMenu');
 });
 
-
-$('.header-item').click(function() {
-    $(this).siblings().removeClass('showList');
-    $(this).toggleClass('showList');
+debt.addEventListener('click', () => {
+  debt.children[2].classList.toggle('d-none');
 });
+
+debtLists.forEach(list => {
+  list.addEventListener('click', () => {
+    wrapperFormInp.value = list.innerText
+  })
+});
+
+let [forIndividualsBtn, forLegalEntitiesBtn] = sliderBtns;
+
+forLegalEntitiesBtn.addEventListener('click', () => {
+  forIndividuals.classList.add('d-none');
+  forLegalEntities.classList.remove('d-none');
+  forLegalEntitiesBtn.classList.add('active-sort');
+  forIndividualsBtn.classList.remove('active-sort');
+});
+forIndividualsBtn.addEventListener('click', () => {
+  forIndividuals.classList.remove('d-none');
+  forLegalEntities.classList.add('d-none');
+  forLegalEntitiesBtn.classList.remove('active-sort');
+  forIndividualsBtn.classList.add('active-sort');
+});
+
 
 $('.mobile-menu__list').click(function() {
   $(this).siblings().removeClass('openList');
